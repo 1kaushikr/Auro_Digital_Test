2a33138468c492a5a281e0c5ad2a29bf1daf6bec
Kaushik <61331511+1kaushikr@users.noreply.github.com>
2022-12-06 22:27:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,71 +4,71 @@ tree = ET.parse('orders.xml')
 root = tree.getroot()
 
 
-class yu:
-    def init(self, naam):
-        self.naam = naam
-        self.buy_buk = []  # max heap
-        self.sell_buk = []  # min heap
+class Book:
+    def init(self, name):
+        self.name = name
+        self.buy_buks = []  # max heap
+        self.sell_buks = []  # min heap
 
 
 time = 0
-buks = {}
+buk = {}
 
 for son in root:
     # add time to each ord
-    son.feature['Time'] = time
-    buk = son.feature['buk']
-    price = float(son.feature['price'])
-    op = son.feature['operation']
-    vol = float(son.feature['volume'])
+    son.attrib['Time'] = time
+    buks = son.attrib['book']
+    price = float(son.attrib['price'])
+    op = son.attrib['operation']
+    vol = float(son.attrib['volume'])
     time += 1
-    if buk not in buks:
-        buks[buk] = yu(buk)
+    if buks not in buk:
+        buk[buks] = Book(buks)
     if son.tag == 'DeleteOrder':
-        if son.feature['operation'] == 'SELL':
-            buks[buk].sell_buk.remove(son.feature['ordId'])
+        if son.attrib['operation'] == 'SELL':
+            buk[buks].sell_buks.remove(son.attrib['orderId'])
         else:
-            buks[buk].buy_buk.remove(son.feature['ordId'])
+            buk[buks].buy_buks.remove(son.attrib['orderId'])
     else:
-        if son.feature['operation'] == 'SELL':
-            for ord in buks[buk].buy_buk:
-                if float(ord.feature['price']) >= price:
-                    if ord.feature['volume'] > vol:
-                        ord.feature['volume'] -= vol
+        if son.attrib['operation'] == 'SELL':
+            for ord in buk[buks].buy_buks:
+                if float(ord.attrib['price']) >= price:
+                    if ord.attrib['volume'] > vol:
+                        ord.attrib['volume'] -= vol
                         vol = 0
                     else:
-                        vol -= int(ord.feature['volume'])
-                        buks[buk].buy_buk.remove(ord)
+                        vol -= int(ord.attrib['volume'])
+                        buk[buks].buy_buks.remove(ord)
                         # sort by price
-                        buks[buk].buy_buk.sort(key=lambda x: float(x.feature['price']), reverse=True)
+                        buk[buks].buy_buks.sort(key=lambda x: float(x.attrib['price']), reverse=True)
                 else:
                     break
             if vol > 0:
-                son.feature['volume'] = vol
-                buks[buk].sell_buk.append(son)
+                son.attrib['volume'] = vol
+                buk[buks].sell_buks.append(son)
                 # sort by price
-                buks[buk].sell_buk.sort(key=lambda x: float(x.feature['price']))
+                buk[buks].sell_buks.sort(key=lambda x: float(x.attrib['price']))
                 break
         else:
-            for ord in buks[buk].sell_buk:
-                if float(ord.feature['price']) <= price:
-                    if ord.feature['volume'] > vol:
-                        ord.feature['volume'] -= vol
+            for ord in buk[buks].sell_buks:
+                if float(ord.attrib['price']) <= price:
+                    if ord.attrib['volume'] > vol:
+                        ord.attrib['volume'] -= vol
                         vol = 0
                     else:
-                        vol -= int(ord.feature['volume'])
-                        buks[buk].sell_buk.remove(ord)
+                        vol -= int(ord.attrib['volume'])
+                        buk[buks].sell_buks.remove(ord)
                         # sort by price
-                        buks[buk].sell_buk.sort(key=lambda x: float(x.feature['price']))
+                        buk[buks].sell_buks.sort(key=lambda x: float(x.attrib['price']))
                 else:
                     break
             if vol > 0:
-                son.feature['volume'] = vol
-                buks[buk].buy_buk.append(son)
+                son.attrib['volume'] = vol
+                buk[buks].buy_buks.append(son)
                 # sort by price
-                buks[buk].buy_buk.sort(key=lambda x: float(x.feature['price']), reverse=True)
+                buk[buks].buy_buks.sort(key=lambda x: float(x.attrib['price']), reverse=True)
                 break
 #
-# We can keep a dictionary of the ord buks that map to the maxheap and minheap for purchase ords and sell ords, respectively. The ord buk now has a second dictionary of ordIDs.
-# When an ord is received, if it is a delete ord, we look for it in the ord buk's piles and, if we find it, delete it.
-# If it is an addord, we add to the add ord heap and continue deleting ords from both buks until the condition is satisfied. This is done until the price at the top of the sale buk is less than the top of the purchase buk.
+# We can keep a dictionary of the ord buk that map to the maxheap and minheap for purchase ords and sell ords, respectively. The ord buks now has a second dictionary of ordIDs.
+# When an ord is received, if it is a delete ord, we look for it in the ord buks's piles and, if we find it, delete it.
+# If it is an addord, we add to the add ord heap and continue deleting ords from both buk until the condition is satisfied. This is done until the price at the top of the sale buks is less than the top of the purchase buks.
